7663898636c1dd7a9d860360cb1b7a620466ff12
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-09-08 14:02:20 +0500

    Commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -11,32 +11,21 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
 
-        List<Character> charList = new ArrayList<>();
+        List<String> byLines = new ArrayList<>();
 
-        try(InputStream input = new FileInputStream(file)) {
-
-            int result;
-            while ((result = input.read()) != -1) {
-                charList.add((char) result);
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                byLines.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        List<String> byLines = new ArrayList<>();
-        StringBuilder line = new StringBuilder();
-        for (char chars : charList) {
-            if (chars == '\n') {
-                byLines.add(line.toString());
-                line = new StringBuilder();
-            }
-            line.append(chars);
-        }
-
         Map<String, String> mapList = new HashMap<>();
         for (String words : byLines) {
             String[] splitWords = words.split(":");
-            mapList.put(splitWords[0], splitWords[1]);
+            mapList.put(splitWords[0].trim(), splitWords[1].trim());
         }
 
         Profile newProfile = new Profile();
